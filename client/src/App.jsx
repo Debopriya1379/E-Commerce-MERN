a19486f03ba9b7ee4748bbd1b452345ab248a1ed
Debopriya1379 <papai7909@gmail.com>
2023-10-08 17:10:12 +0530

    frontend completed

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+// import Cart from './pages/Cart';
+// import Login from './pages/Login';
+// import Product from './pages/Product';
+import Home from './pages/Home';
+// import ProductList from './pages/ProductList';
+// import Register from './pages/Register';
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+      {/* <ProductList/> */}
+      {/* <Product/> */}
+      {/* <Register/> */}
+      {/* <Login/> */}
+      {/* <Cart/> */}
+    </div>
+  );
+}
+
+export default App;
